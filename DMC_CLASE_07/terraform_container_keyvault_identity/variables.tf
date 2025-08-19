5587ce6233f103cf6d8be156c9c727eeea1eb4e6
hansleolml@gmail.com <hansleolml@gmail.com>
2025-08-18 19:07:32 -0500

    se añade container y keyvault

---
+++ DMC_CLASE_07/terraform_container_keyvault_identity/variables.tf
@@ -0,0 +1,37 @@
+variable "location" {
+  description = "La ubicación donde se creará el componente"
+  type        = string
+}
+
+variable "tags" {
+  description = "Etiquetas opcionales para aplicar al componente"
+  type        = map(string)
+  default = {
+    Environment = ""
+  }
+}
+
+// RESOURCE GROUP 01------------------------------- 
+variable "rg_01_name" {
+  description = "El nombre del grupo de recursos 1"
+  type        = string
+}
+
+// Container Apps(ca) & Environments(cae) 01
+
+variable "cae_01_workload_profile" {
+  description = "El Workload del cae 01"
+  type        = string
+}
+
+variable "cae_01_name" {
+  description = "El nombre del cae 01"
+  type        = string
+}
+
+variable "aca_01_name" {
+  description = "El nombre del aca 01"
+  type        = string
+}
+
+
