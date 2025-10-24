436ab7719306050aa63f0c1541d5a5c2aa63a4db
leo <leo@MacBook-Pro-de-Hans.local>
2025-10-23 21:31:28 -0500

    add dmc_ejemplo

---
+++ DMC_EJEMPLO/resource_group.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg_01" {
+  name     = "rg-dmc-dev-001-hans"
+  location = "East US 2"
+}
+
+resource "azurerm_resource_group" "rg_02" {
+  name     = "rg-dmc-dev-002-hans"
+  location = "East US 2"
+}
