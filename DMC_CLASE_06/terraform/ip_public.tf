0d0f4d90f3d84f1d9ac0a02bbd428fa95a4d3a14
leo <leo@MacBook-Pro-de-Hans.local>
2025-11-06 19:17:31 -0500

    update

--- DMC_CLASE_06/terraform/ip_public.tf
+++ DMC_CLASE_06/terraform/ip_public.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "ip_01" {
   name                = "ip-dmcexam-dev-eastus2-001"
-  location            = "East US 2"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg_01.name
   allocation_method   = "Static"
 }
