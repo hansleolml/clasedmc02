3b3a9f656b54b6e10fb24b7c5715cc590def776f
leo <leo@MacBook-Pro-de-Hans.local>
2026-07-20 21:42:13 -0500

    update

--- DMC_CLASE_02/VNET-RG/provider.tf
+++ DMC_CLASE_02/VNET-RG/provider.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "=4.4.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  resource_provider_registrations = "none"
   features {}
+  subscription_id = "b497fd69-266c-46a9-b55b-8be0cd579667"
 }
